8ef7adcdb18db87fbf2f3161a91c8cb2e98770c7
NilSagor <masum1201@gmail.com>
2016-02-19 15:16:21 +0600

    inlcudes folder

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Blood donor</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/custom.css">
+	<script type="text/javascript">
+    $(".form_datetime").datetimepicker({
+        format: "dd MM yyyy - hh:ii",
+        autoclose: true,
+        todayBtn: true,
+        pickerPosition: "bottom-left"
+    }'show');
+</script> 
+
+</head>
+<body>
+	<?php include('includes/navbar.php'); ?>
+
---
+++ navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container-fluid">
+		<!-- brand -->
+		<div class="navbar-header">
+			<button class="navbar-toggle collapse" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>				
+			</button>
+			<a href="index.php" class="navbar-brand">Blood Donation</a>
+		</div>
+		<!-- /navbar-header -->
+		<!-- navbar-link -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+				<li><a href="all_user.php">All User</a></li>
+				<li><a href="about.php">About us</a></li>
+				<li><a href="register.php">Register</a></li>
+				<li><a href="" data-toggle="modal" data-target="#modal-contact-form">Contact us</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
